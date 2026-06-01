f8027ec87bf54562a61d43310cf0ac3cd604f6b8
Joshua Crass <joshua.crass@gmail.com>
2026-06-01 07:42:04 -0700

    chore: strip titan section components, replace Home with placeholder

--- src/components/AboutSection.jsx
+++
@@ -1,6 +0,0 @@
-// Placeholder - About section will be redesigned separately
-function AboutSection() {
-  return null;
-}
-
-export default AboutSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,33 @@
 import styled from '@emotion/styled';
-import HeroSection from 'components/Hero';
-import Services from 'components/Services';
-// import AboutSection from 'components/AboutSection';
-// import TeamSection from 'components/TeamSection';
-// import ProcessSection from 'components/ProcessSection';
-// import ServiceAreaSection from 'components/ServiceAreaSection';
-import WhySection from 'components/WhySection';
-// import FAQSection from 'components/FAQSection';
-// import ContactFormSection from 'components/ContactFormSection';
 import Footer from 'components/Footer';
 
 const HomeWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 2rem;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.25rem;
+  text-align: center;
+  opacity: 0.7;
+  margin-top: 1rem;
 `;
 
 function Home() {
   return (
     <HomeWrapper>
-      <HeroSection />
-      <Services />
-      {/* <AboutSection /> */}
-      {/* <TeamSection /> */}
-      {/* <ProcessSection /> */}
-      {/* <ServiceAreaSection /> */}
-      <WhySection />
-      {/* <FAQSection /> */}
-      {/* <ContactFormSection /> */}
+      <Title>Your App Name</Title>
+      <Subtitle>Run yarn setup to configure this project.</Subtitle>
       <Footer />
     </HomeWrapper>
   );
